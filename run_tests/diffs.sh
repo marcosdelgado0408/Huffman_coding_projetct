5c5bffe5837b09d2ee9f8fb19030aa1c9742c156
Marcos Delgado <marcosdelgado0408@gmail.com>
2019-10-16 19:51:42 -0300

    Adicionando shell scrips para rodar os testes mais rapidos

---
+++ run_tests/diffs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+diff -q -s resultados/teste1.txt testes/teste1.txt 
+
+diff -q -s resultados/teste2.txt testes/teste2.txt 
+
+diff -q -s resultados/teste3.txt testes/teste3.txt 
+
+diff -q -s resultados/teste4.txt testes/teste4.txt 
+
+diff -q -s resultados/teste5.txt testes/teste5.txt 
+
+diff -q -s resultados/teste6.txt testes/teste6.txt 
+
+diff -q -s resultados/teste7.txt testes/teste7.txt 
+
+diff -q -s resultados/teste8.txt testes/teste8.txt 
+
+diff -q -s resultados/teste9.txt testes/teste9.txt 
+
+diff -q -s resultados/teste10.txt testes/teste10.txt 
+
+
